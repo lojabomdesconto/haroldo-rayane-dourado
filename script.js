1b3b2ecee4b1b19f7191d71533d274c0a293fadf
lojabomdesconto <81715476+lojabomdesconto@users.noreply.github.com>
2026-09-24 15:07:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
       e.preventDefault();
 
       // Digite aqui o número do WhatsApp que vai receber os dados (DDD + número)
-      const numeroWhatsapp = "5511999999999"; 
+      const numeroWhatsapp = "553492995097"; 
 
       const nome = document.getElementById('nome').value;
       const celebracao = document.getElementById('presencaCelebracao').value;
